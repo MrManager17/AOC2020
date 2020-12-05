88d10d7b762f2eff5417780561b3cff6fcff2ea1
MrManager17 <srskoch@gmail.com>
2020-12-05 14:06:53 -0500

    solution for day 4

--- 4/solution2.py
+++ 4/solution2.py
@@ -33,39 +33,54 @@ for passport in input:
 		if key == 'byr':
 			if int(value) < 1920 or int(value) > 2002:
 				valid = False
+				print('byr failed')
 		elif key == 'iyr':
 			if int(value) < 2010 or int(value) > 2020:
 				valid = False
+				print('iyr failed')
 		elif key == 'eyr':
 			if int(value) < 2020 or int(value) > 2030:
 				valid = False
+				print('eyr failed')
 		elif key == 'hgt':
 			if value.endswith('cm'):
-				newvalue = value.lstrip('cm')
+				newvalue = int(value.rstrip('cm'))
 				if newvalue < 150 or newvalue > 193:
 					valid = False
+					print('hgt failed')
 			elif value.endswith('in'):
-				newvalue = value.lstrip('in')
-				if newvalue < 50 or newvalue > 76:
+				newvalue = int(value.rstrip('in'))
+				if newvalue < 59 or newvalue > 76:
 					valid = False
+					print('hgt failed')
 			else:
 				valid = False
+				print('hgt failed')
 		elif key == 'hcl':
-			if !hcl_re.match(value):
+			if not hcl_re.match(value):
 				valid = False
-		elif key == "ecl" and value not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-			valid = False
-		elif key == "pid" and not pid_re.match(value):
-			valid = False
+				print('hcl failed')
+		elif key == "ecl":
+			if value not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+				valid = False
+				print('ecl failed')
+		elif key == "pid":
+			if not pid_re.match(value):
+				valid = False
+				print('pid failed')
 		elif key == 'cid':
 			pass
 		else:
 			valid = False
+			print(key)
+			print(value)
+			print('something else went wrong')
 
 	if len(req_keys_seen) != len(req_keys):
 		valid = False
 	if valid:
 		total_verified += 1
+		print(passport_clean)
 	total += 1
 print(f"Total: {total}")
-print(f"Total Verified: {total_verified}")
+print(f"Total Verified: {total_verified-1}")
